1979c0413ff72ca9d123e16f168728e6bb1c007a
JTok <jtok@users.noreply.github.com>
2019-12-06 23:48:28 -0600

    added check for posts and args from bash

--- source/functions.php
+++ source/functions.php
@@ -63,4 +63,31 @@
     return $script_contents;
   }
 
+  // check for post commands.
+  // if update_script_contents exists, then update the user script file.
+  if (isset($_POST['#update_script_contents'])) {
+    $default_script_file = $_POST['#default_script_file'];
+    $user_script_file = $_POST['#user_script_file'];
+    $conf_file = $_POST['#conf_file'];
+
+    // create a variable with the default script contents and user config file merged.
+    $script_contents = update_script_contents($default_script_file, $user_conf_file);
+
+    // write script contents variable as the user script file.
+    file_put_contents($user_script_file, $script_contents);
+  }
+
+  // check for arguments passed from bash.
+  // if first argument is update_user_script, then update the user script file.
+  if ($argv[1] == "update_user_script") {
+    $default_script_file = $argv[2];
+    $user_script_file = $argv[3];
+    $conf_file = $argv[4];
+
+    // create a variable with the default script contents and user config file merged.
+    $script_contents = update_script_contents($default_script_file, $user_conf_file);
+
+    // write script contents variable as the user script file.
+    file_put_contents($user_script_file, $script_contents);
+  }
 ?>
