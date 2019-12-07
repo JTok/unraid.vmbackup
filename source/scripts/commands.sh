4a1d05a91e177105189a8690e95a338f739fe723
JTok <jtok@users.noreply.github.com>
2019-12-06 23:49:12 -0600

    file for plugin bash commands

---
+++ source/scripts/commands.sh
@@ -0,0 +1,46 @@
+#!/bin/bash
+#
+# usage: update_user_script
+
+
+#### start script variables ####
+
+  # update user script
+  update_user_script () {
+    # create local variables.
+    local php_functions_script="/usr/local/emhttp/plugins/vmbackup/functions.php"
+    local default_script="/usr/local/emhttp/plugins/vmbackup/default.cfg"
+    local user_script="/boot/config/plugins/vmbackup/user-script.sh"
+    local user_config="/boot/config/plugins/vmbackup/user.cfg"
+
+    # verify the default script and the user config files exist.
+    if [[ -f "$default_script" ]] && [[ -f "$user_config" ]]; then
+      
+      # if the a user script already exists, remove it.
+      if [[ -f "$user_script" ]]; then
+        rm -fv "$user_script"
+      fi
+
+      php "$php_functions_script" "update_user_script" "$default_script" "$user_script" "$user_config"
+
+    else
+
+      echo "could not find default script and/or user config."
+    fi
+  }
+
+#### end script variables ####
+
+
+#### start script execution ####
+
+  case "$1" in
+    'update_user_script')
+      update_user_script
+      ;;
+    *)
+     echo "usage $0 update_user_script"
+     ;;
+  esac
+
+#### end script execution ####
