eb1f034387c89a758fe2eac31f0435d8aaebf5e5
Justin Tarr <1851873+JTok@users.noreply.github.com>
2020-01-02 22:53:33 -0600

    added validation for uploading scripts

--- source/include/validation.php
+++ source/include/validation.php
@@ -3,7 +3,6 @@
   /* vmbackup plugin
     copyright 2019 JTok */
 
-
   // function to validate form options.
   function validate_form() {
     return "form validated";
@@ -22,5 +21,126 @@
 // function to validate if a string follows a absolute linux file naming path convention with trailing slashes.
   function is_absolute_path($path) {
     return (preg_match("(^\/$)|(^\/([\w-]*)+(\/[\w-]*)*[\/]$)", $path));
+  }
+
+  // function to validate user pre and post scripts.
+  function validate_script($script_contents, $conf_file) {
+    // replace line breaks with unix style line breaks.
+    $script_contents = replace_line_breaks($script_contents);
+    // parse config file.
+    $conf = parse_ini_file($conf_file);
+
+    // create empty string for pre-pending.
+    $preprend = "";
+    // read the first line of the script.
+    $line = rtrim(strtok($script_contents, "\n"));
+    if (!strcmp($line, "#!/bin/bash") == 0) {
+      // if shebang is missing, add it to string to be pre-pended.
+      $shebang_exists = false;
+      $prepend = "#!/bin/bash\n";
+    } else {
+      $shebang_exists = true;
+    }
+    // see if the file contains any special variables.
+    $special_conf_array = get_special_variables($script_contents, 10, false);
+    // if the file does not contain any special variables, add them.
+    if (empty($special_conf_array)) {
+      $prepend .= "#arrayStarted=true\n";
+      if ($conf["noParity"] == "false") {
+        $prepend .= "#noParity=false\n";
+      } else {
+        $prepend .= "#noParity=true\n";
+      }
+    } else {
+      if (!empty($special_conf_array["arrayStarted"])) {
+        $script_contents = replace_line("arrayStarted", "", $script_contents, false);
+        $prepend .= "#arrayStarted=true\n";
+      } else {
+        $prepend .= "#arrayStarted=true\n";
+      }
+      if (!empty($special_conf_array["noParity"])) {
+        $script_contents = replace_line("noParity", "", $script_contents, false);
+        $prepend .= "#noParity=" . $conf["noParity"];
+      } else {
+        $prepend .= "#noParity=" . $conf["noParity"];
+      }
+    }
+    // prepend the string to the file.
+    $prepend = rtrim($prepend);
+    if (!empty($prepend) && $shebang_exists == false) {
+      $script_contents = prepend_string($prepend, $script_contents, false);
+    } elseif (!empty($prepend) && $shebang_exists == true ) {
+      $script_contents = replace_line("#!/bin/bash", "", $script_contents, false);
+      $prepend = "#!/bin/bash\n" . $prepend;
+      $script_contents = prepend_string($prepend, $script_contents, false);
+    }
+    return $script_contents;
+  }
+
+  // function to replace line in a file based on a search string.
+  function replace_line($search_string, $new_line, $contents, $is_file = true) {
+    // check to see if the contents passed are a file or not.
+    if ($is_file == true) {
+      $file = $contents;
+      $reading = fopen($file, 'r');
+      $writing = fopen($file . ".tmp", 'w');
+
+      $replaced = false;
+
+      while (!feof($reading)) {
+        $line = fgets($reading);
+        if (stristr($line, $search_string)) {
+          if ($new_line == "") {
+            $line = $new_line;
+          } else {
+            $line = $new_line . "\n";
+          }
+          $replaced = true;
+        }
+        fputs($writing, $line);
+      }
+      fclose($reading); fclose($writing);
+      // don't overwrite the file if nothing was replaced.
+      if ($replaced) {
+        rename($file . ".tmp", $file);
+      } else {
+        unlink($file . ".tmp");
+      }
+    } elseif ($is_file == false) {
+      // check to see if the variable is a string or an array.
+      if (gettype($contents) == "array") {
+        // if it is an array, copy it to contents_array.
+        $contents_array = $contents;
+      } elseif (gettype($contents) == "string") {
+        // if it is a string, explode it into an array using newline.
+        $contents_array = explode("\n", $contents);
+      }
+      // search array values for the search string and return the corresponding key.
+      // use regex to find the full search string value.
+      $search_string = preg_quote($search_string, '/');
+      $search_result_array = preg_grep("/.*$search_string.*/", $contents_array);
+      // remove empty values.
+      $search_result_array = array_values(array_filter($search_result_array));
+      // get the full search string.
+      $full_search_string = $search_result_array[0];
+      $key = array_search($full_search_string, $contents_array);
+      // replace the line in the contents array with the new one.
+      if ($new_line == "" ) {
+        unset($contents_array[$key]);
+        $contents_array = array_values($contents_array);
+      } else {
+        $contents_array[$key] = $new_line;
+      }
+
+      // check to see if the variable is a string or an array.
+      if (gettype($contents) == "array") {
+        // if it is an array, return an array.
+        return $contents_array;
+      } elseif (gettype($contents) == "string") {
+        // if it is a string, return a string.
+        $contents_string = implode("\n", $contents_array);
+        return $contents_string;
+      }
+    }
   }
 ?>
