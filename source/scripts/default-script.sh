b799dfd87915ce424e2d11cd3bff0b12b1bab4cd
Justin Tarr <1851873+JTok@users.noreply.github.com>
2020-01-20 23:36:54 -0600

    update version

--- source/scripts/default-script.sh
+++ source/scripts/default-script.sh
@@ -2,7 +2,7 @@
 #backgroundOnly=true
 #arrayStarted=no_config
 #noParity=no_config
-#version=v0.2.0 - 2020/01/09
+#version=v0.2.0 - 2020/01/21
 
 # based on unraid-vmbackup script version:
 # v1.2.3 - 2019/12/24
